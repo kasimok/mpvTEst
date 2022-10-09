e9e51fd08fd1282b9e9c9c232cf2a4f79dfc55d6
kakaiikaka <kasimok@163.com>
2022-10-09 10:50:44 +0800

    Initial Commit

---
+++ mpvTEst/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  mpvTEst
+//
+//  Created by evilisn_jiang on 9/28/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ mpvTEst/mpvTEst-BridgingHeader.h
@@ -0,0 +1,4 @@
+#import <mpv/client.h>
+#import <mpv/render.h>
+#import <mpv/render_gl.h>
+#import <mpv/stream_cb.h>
